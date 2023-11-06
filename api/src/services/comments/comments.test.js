6a4b43c8a639b6e43c9290fcbab41f49defa7af7
Your name <you@example.com>
2023-11-05 23:00:28 -0500

    Chapter 6 final commit

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,3 +1,5 @@
+import { db } from 'src/lib/db'
+
 import { comments, createComment } from './comments'
 
 // Generated boilerplate tests do not account for all circumstances
@@ -7,11 +9,18 @@ import { comments, createComment } from './comments'
 // https://redwoodjs.com/docs/testing#jest-expect-type-considerations
 
 describe('comments', () => {
-  scenario('returns all comments', async (scenario) => {
-    const result = await comments()
+  scenario(
+    'returns all comments for a single post from the database',
+    async (scenario) => {
+      const result = await comments({ postId: scenario.comment.jane.postId })
+      const post = await db.post.findUnique({
+        where: { id: scenario.comment.jane.postId },
+        include: { comments: true },
+      })
+      expect(result.length).toEqual(post.comments.length)
+    }
+  )
 
-    expect(result.length).toEqual(Object.keys(scenario.comment).length)
-  })
   scenario('postOnly', 'creates a new comment', async (scenario) => {
     const comment = await createComment({
       input: {
@@ -22,10 +31,10 @@ describe('comments', () => {
         },
       },
     })
-
-    expect(comment.name).toEqual('Billy Bob')
-    expect(comment.body).toEqual('What is your favorite tree bark?')
-    expect(comment.postId).toEqual(scenario.post.bark.id)
-    expect(comment.createdAt).not.toEqual(null)
   })
+
+  expect(comment.name).toEqual('Billy Bob')
+  expect(comment.body).toEqual('What is your favorite tree bark?')
+  expect(comment.postId).toEqual(scenario.post.bark.id)
+  expect(comment.createdAt).not.toEqual(null)
 })
